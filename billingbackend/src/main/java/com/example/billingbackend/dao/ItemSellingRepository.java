0f57ff89ad833e2109757e01da953d272ec9d30f
Ashwanigupta9125 <ashwanigameup@gmail.com>
2023-10-05 22:41:04 +0530

    commit with analysis with date

--- billingbackend/src/main/java/com/example/billingbackend/dao/ItemSellingRepository.java
+++ billingbackend/src/main/java/com/example/billingbackend/dao/ItemSellingRepository.java
@@ -1,11 +1,22 @@
 package com.example.billingbackend.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import com.example.billingbackend.entities.ItemSelling;
 
 @Repository
 public interface ItemSellingRepository extends JpaRepository<ItemSelling, Integer>{
 
+	
+	@Query(value ="select * from item_selling where date =:date",nativeQuery = true)
+	public List<ItemSelling>  FindTransitionOfBuy(@Param("date") LocalDate date);
+	
+	@Query(value ="select * from item_selling where date BETWEEN :startDate AND :endDate",nativeQuery = true)
+	public List<ItemSelling>  FindTransitionOfBuyBtw(@Param("startDate") LocalDate startDate,@Param("endDate") LocalDate endDate);
+	
 }
